e2e15b01bdd9994df7d36a04c1541373a9a15dc7
Jisse Reitsma <jisse@yireo.com>
2019-06-27 09:06:09 +0200

    Initial files

---
+++ README.md
@@ -0,0 +1,2 @@
+# Yireo ReactDevelopment module for Magento 2
+See https://github.com/yireo-training/Yireo_React
---
+++ view/base/requirejs-config.js
@@ -0,0 +1,8 @@
+var config = {
+    map: {
+        '*': {
+            react: 'Yireo_ReactDevelopment/js/react.development',
+            reactDom: 'Yireo_ReactDevelopment/js/react-dom.development'
+        }
+    }
+};
